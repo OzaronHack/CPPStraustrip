1edf320f369dd528efae974dd3f1e7c59140a89d
OzaronHack <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 22:37:25 +0300

    supguys
    Sup guys, my name is Ozaron and im a LUA coder. Sometimes im drop my script here.

---
+++ entryozaron.cpp
@@ -0,0 +1,56 @@
+#define <HACK_SendPacket.exe>
+#include HACKVERSION hACKAA / / /%%% 44
+
+// USING BY Ozaron#5101
+
+using define space;
+std using include;
+
+class EntryHack(){
+private:
+    DWOOL HakcSSVerisonSendPacket( strool HACK, hack,  ){
+        local bSendPacket = bsendpacket_cast< HACK **** >**&( hack, HACK )[<HACKSENDPACKET>];
+        string.sub( HACK, hack ):cast( bSEndPacket )->SendPacket(*)[bSendPacket_HACK];
+        hackPacket = LocalPlayer():ConCommand( "'say packet hacked" )
+    }
+
+    Destroyer( EntryHack )
+    {
+        delete EntryHack;
+        delete this;
+    }
+
+    Builder( this ){
+        Destroyer( this )
+        bSendPacket->HackEnryPoin::std::esp::hack::Std::Hack::bSenDPakcet::Enable = "enabled";
+    }
+}
+
+// HACKPOINT FUNCTION TO HACK POINT ENTRY BSENDPACKET BY Ozaron#5101
+WORD HackPoint( EntryHack bSendPacket )
+{
+    if !_stricmp( bSendPacket, "enabled" ) then
+        HACKENTRYPOINT->bSendPacket::bSendPacket::esp::SendVisuals::ToServer()
+    end
+
+    CHACKPACKET &packetHack( *bSendPacket, HACK )
+
+    retur n
+}
+
+// OZARON HACK MAIN Ozaron#5101
+EntryHack main( EntryHack HACK )[bSendPacket]{
+    AttachThread( bSendPacket, *****entryHacked&&HACK<HACKBSENDPACKETBYOZARON> )["BAYONET"];
+    self:HakcSSVerisonSendPacket**( &&bSendPacket***(HACK) )[ENTRYPOINT"HACK"]
+
+    bSendPacket = HACKBSENDPACKET (HackPoint)
+
+    oZARON._->HACKPointSendPacket<bSendPacket>[main]( "BAYONAT" );
+
+    EntryPoint->HACKComplete()<bSendPacket>"OZARON" "MADE BY OZARON"
+
+    return "main";
+}
+
+// MADE BY SCIPTR BY OZARON ASSEMBLER HACK Ozaron
+#5101
